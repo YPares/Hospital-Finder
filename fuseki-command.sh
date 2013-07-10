efb3c0d225dd15d15d09570cca954516b4c1c004
YPares <yves.pares@gmail.com>
2013-07-10 17:15:27 +0200

    Fuseki command added

--- fuseki-command.sh
+++ fuseki-command.sh
@@ -1,1 +1,3 @@
+#You need to have downloaded the Fuseki html pages before and put
+#them into a 'fuseki-pages' folder
 lein run -m org.apache.jena.fuseki.FusekiCmd --pages=fuseki-pages --update --mem ~/here
